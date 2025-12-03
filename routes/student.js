75dfc6eb9c26d69909e3790d65cf46295f125769
muthukumar-uxmint <muthukumar.k@uxmint.in>
2025-12-03 11:41:56 +0530

    crud for students

--- routes/student.js
+++ routes/student.js
@@ -32,4 +32,77 @@ router.get('/students' , async (req, res, next)=>{
     }
 });
 
+router.post('/students' , async (req , res ,next)=>{
+    try{
+
+        const { student_id , student_name , student_age } = req.body;
+        const query = `
+            INSERT INTO students VALUES ( $1 , $2 , $3) RETURNING student_name;
+        `;
+
+        const result = await sequelize.query(query , {
+            bind:[student_id , student_name , student_age]
+        });
+
+        console.log(result[0]);
+
+        res.json({ success:true , result});
+
+    }catch(e){
+        console.log(e);
+        next(e);
+    }
+});
+
+router.put('/students' , async (req , res ,next)=>{
+    try{
+
+        const { student_id , student_name } = req.body;
+        const query = `
+            UPDATE students SET student_name = :student_name WHERE student_id = :student_id RETURNING *;
+        `;
+
+        const result = await sequelize.query(query , {
+            replacements : {
+                student_id : student_id,
+                student_name : student_name
+            }
+        });
+
+        console.log(result[0]);
+
+        res.json({ success:true , result});
+
+    }catch(e){
+        console.log(e);
+        next(e);
+    }
+});
+
+router.delete('/students' , async (req , res ,next)=>{
+    try{
+
+        const { student_id  } = req.body;
+        const query = `
+            DELETE FROM students WHERE student_id = :student_id RETURNING *;
+        `;
+
+        const result = await sequelize.query(query , {
+            replacements : {
+                student_id : student_id
+            }
+        });
+
+        console.log(result[0]);
+
+        res.json({ success:true , result});
+
+    }catch(e){
+        console.log(e);
+        next(e);
+    }
+});
+
+
+
 module.exports = router;
